c06e2edb971cb38c627942e46661626311f40b64
cheonhy25u <kor01chs@gmail.com>
2024-12-06 16:04:07 +0900

    basic add

---
+++ basic/dataType.py
@@ -0,0 +1,12 @@
+integer = 1
+float = 3.14
+boolean = True
+string = "hello"
+list = [1, 3.14, True, "hello"]
+
+print(type(integer), integer)
+print(type(float), float)
+print(type(boolean), boolean)
+print(type(string), string)
+print(type(list), list)
+print(type(list), *list) #Asterisk로 리스트를 압축 해제할 수 있다고 한다.
---
+++ basic/printWithX.py
@@ -0,0 +1,5 @@
+#String 이외의 데이터들과 함께 출력
+a = 1
+b = 2
+print(a,'+',b,'=',a+b)
+print(f"{a} + {b} = {a+b}")
