6b68abf2401a00666fba448be00e0b28ec7529aa
1000hy25u <kor01chs@gmail.com>
2024-12-09 18:43:49 +0900

    list append

---
+++ basic/basic_list.py
@@ -0,0 +1,7 @@
+squares = [1, 4, 9, 16, 25]
+
+print(squares[0])
+print(squares[-1])
+print(squares[:3])
+print(squares[-3:])
+print(squares[:-3])
---
+++ basic/changeAndAppend.py
@@ -0,0 +1,6 @@
+a = [1,2,3,4,5]
+a.append(6)
+print(a)
+
+a[0] = 0
+print(a)
